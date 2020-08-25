296d5b7b9cac5eb39db1f9cb1d8ec1f8aee941c0
DDA209 <didier.pascarel1@gmail.com>
2020-08-25 12:45:40 +0200

    continue

--- src/components/core/Wallets.js
+++ src/components/core/Wallets.js
@@ -1,12 +1,37 @@
 import React from 'react';
+import { 
+    Row,
+    Col,
+ } from 'react-bootstrap';
 
 class Wallets extends React.Component {
 
     render(){
         return(
-            <div>
-                <h2>Portefeuilles crypto</h2>
-            </div>
+
+            <Row>
+
+                <Col xs={12}>
+                    <h2>Portefeuilles crypto</h2>
+                </Col>
+                <Col
+                    xs={2}
+                    sm={2}
+                    md={2}
+                    lg={2}
+                    xl={2}
+
+
+
+                >
+                    <Table
+                        contents = "coins"
+                        table = {this.state.table}
+
+                    />
+                </Col>
+            </Row>
+
         )
     }
 }
